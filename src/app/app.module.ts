64ebff442eddb8d889501a5cee08cbe3d8c816ba
obewas <obewas1202@gmail.com>
2020-08-24 18:25:41 +0300

    added quote details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote'
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+   votes: number
+   @Input () quote:Quote
+  constructor() { 
+    this.votes += 1
+  }
+  likeVote(): boolean{
+    this.votes +=1;
+    return false;
+  }
+
+  dislikeVote(): boolean{
+   this.votes -=1;
+   return false;
+ }
+  
+   
+  ngOnInit(): void {
+  }
+
+}
