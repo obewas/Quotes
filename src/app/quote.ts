58350407e00b9e0bdef85e4c3762f875d2a08078
obewas <obewas1202@gmail.com>
2020-08-24 19:54:29 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    public showAuthor:boolean;
-    constructor(public id:number, public name:string,public author:string,public submitted:string){
-        this.showAuthor=false
-    }
-}
