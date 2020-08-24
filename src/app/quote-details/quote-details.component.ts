2edc1caa5436fc705e2b5883d1843ae5d9912207
obewas <obewas1202@gmail.com>
2020-08-24 18:51:26 +0300

    added bootstrap css

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
 import { Quote } from '../quote'
 @Component({
   selector: 'app-quote-details',
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,11 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-  new Quote (1, 'There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will.', 'Epictetus', 'John')
+  new Quote (1, 'There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will.', 'Epictetus', 'John'),
+  new Quote(2, 'The moon is beautiful.', 'B.C Smith',"E.I"),
+  new Quote(3, 'The earth is meh.', 'D.E Jacobsons',"T.O"),
+  new Quote(4, 'The galaxy is large.', 'F.G Randos',"P.S"),
+  new Quote(5, 'The universe is expanding.', 'I.J Persons',"Q.T"),
   ];
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
