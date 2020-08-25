f5f92a9e0aded474ab616335069fab3f8e576e0a
obewas <obewas1202@gmail.com>
2020-08-25 20:13:36 +0300

    origin master

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
   quotes = [
   new Quote ('JP', 'There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will.', 'Epictetus'),
-  new Quote('JP', 'The two most powerful warriors are patience and time.', 'Leo Tolstoy'),
-  new Quote('JP', 'Time is money', 'Benjamin Franklin'),
-  new Quote('JP','The galaxy is large.', 'F.G Randos'),
-  new Quote('JP', 'The universe is expanding.', 'I.J Persons'),
+  new Quote('MN', 'The two most powerful warriors are patience and time.', 'Leo Tolstoy'),
+  new Quote('BF', 'Time is money', 'Benjamin Franklin'),
+  new Quote('FR','Lost time is never found again.', 'F.G Randos'),
+  new Quote('IP', 'The universe is expanding.', 'I.J Persons'),
   ];
   addNewQuote(addedQuote){
     this.quotes.push(addedQuote)
