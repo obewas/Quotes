104733d63baed5d1e8b7074fd65e3645f5d08d32
obewas <obewas1202@gmail.com>
2020-08-25 20:57:21 +0300

    added date count pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { DateCountPipe } from './date-pipe/date-count.pipe';
+import { DateCountPipe } from './date-count.pipe';
 
 
 @NgModule({
